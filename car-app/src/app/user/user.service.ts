903652704a4c43d73112cbf64f27a4e6b49fa196
Alexander-DA <alex.alexandrov200@gmail.com>
2024-11-27 19:59:23 +0200

    Added routing to other pages

---
+++ car-app/src/app/user/user.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor() { }
+
+  // TODO: Safe if the user is authenticated in local storage
+}
