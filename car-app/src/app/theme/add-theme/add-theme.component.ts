96afecc0d213689c6d084d2ea419b12795db9697
Alexander-DA <alex.alexandrov200@gmail.com>
2024-12-03 19:30:09 +0200

    Added edit profile view and error handling for it

--- car-app/src/app/theme/add-theme/add-theme.component.ts
+++ car-app/src/app/theme/add-theme/add-theme.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../../api.service';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,26 +13,32 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './add-theme.component.html',
-  styleUrl: './add-theme.component.css'
+  styleUrl: './add-theme.component.css',
 })
 export class AddThemeComponent {
   form = new FormGroup({
-    themeName: new FormControl('', [Validators.required, Validators.minLength(5)]),
-    postText: new FormControl('', [Validators.required, Validators.minLength(10)])
-  })
+    themeName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+    postText: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]),
+  });
 
-  isFieldTextMissing(controlName: string) { 
+  isFieldTextMissing(controlName: string) {
     return (
       this.form.get(controlName)?.touched &&
       this.form.get(controlName)?.errors?.['required']
-    )
+    );
   }
   isNotMinLength(controlName: string) {
     return (
       this.form.get(controlName)?.touched &&
       this.form.get(controlName)?.errors?.['minlength']
-    )
-  } 
+    );
+  }
 
   constructor(private apiService: ApiService) {}
 
--- car-app/src/app/user/profile/profile.component.ts
+++ car-app/src/app/user/profile/profile.component.ts
@@ -1,12 +1,85 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import {
+  emailValidator,
+  usernameValidator,
+} from '../../utils/userInputFields.validator';
+import { EMAIL_DOMAINS } from '../../constants';
+import { ProfileDetails } from '../../types/user';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
 export class ProfileComponent {
+  isEditMode: boolean = false;
 
+  profileDetails: ProfileDetails = {
+    username: 'JohnDoe',
+    email: 'john.doe@gmeil.com',
+    tel: '123-123-123',
+  };
+
+  form = new FormGroup({
+    username: new FormControl(this.profileDetails.username, [
+      Validators.required,
+      Validators.minLength(5),
+      usernameValidator(),
+    ]),
+    email: new FormControl(this.profileDetails.email, [
+      Validators.required,
+      emailValidator(EMAIL_DOMAINS),
+    ]),
+    tel: new FormControl(this.profileDetails.tel, [
+      Validators.required,
+      Validators.minLength(9),
+    ]),
+  });
+
+  isFieldTextMissing(controlName: string) {
+    return (
+      this.form.get(controlName)?.touched &&
+      this.form.get(controlName)?.errors?.['required']
+    );
+  }
+
+  isNotMinLength(controlName: string) {
+    return (
+      this.form.get(controlName)?.touched &&
+      this.form.get(controlName)?.errors?.['minlength']
+    );
+  }
+
+  isRegExInvalid(controlName: string) {
+    return (
+      this.form.get(controlName)?.touched &&
+      this.form.get(controlName)?.errors?.[`${controlName}Validator`]
+    );
+  }
+
+  toggleEditMode() {
+    this.isEditMode = !this.isEditMode;
+  }
+
+  saveEditedProfile() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.profileDetails = this.form.value as ProfileDetails;
+    this.toggleEditMode();
+  }
+
+  onCancel(event: Event) {
+    event.preventDefault();
+    this.toggleEditMode();
+  }
 }
